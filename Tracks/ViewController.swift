25130d1c270608419b0f38ad4d67fb4651345788
jpgsloan <jpgsloan@gmail.com>
2015-02-25 17:41:23 -0800

    Initial Commit

---
+++ Tracks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tracks
+//
+//  Created by John Sloan on 2/25/15.
+//  Copyright (c) 2015 JPGS inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
